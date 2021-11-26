3104e7e3f8d3e19a05a293c4d00c76634841c4df
mhagdorn <magnus.hagdorn@marsupium.org>
2021-11-26 12:49:13 +0000

    add setup script
     * .gitignore: ignore some more files
     * README.md: update documentation
     * setup.py: add setup script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# lsdfailtools-web
+lsdfailtools-web
+================
 Web app for running lsdfailtools
+
+Setup
+-----
+Initialise the database by running
+```
+lsdfailtools-manage --init-db
+```
+
+Development
+-----------
+This flask application is developed following the excellent [Flask Mega Tutorial](https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world) and the [Flask Google Login Tutorial](https://realpython.com/flask-google-login/). 
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+name = 'lsdfailtools-web'
+version = '0.0'
+release = '0.0.0'
+author = 'Magnus Hagdorn'
+
+setup(
+    name=name,
+    packages=find_packages(),
+    version=release,
+    include_package_data=True,
+    install_requires=[
+        "sqlalchemy",
+        "flask>=1.0",
+        "flask_sqlalchemy",
+        "flask-login",
+        "oauthlib",
+        "pyOpenSSL"
+    ],
+    extras_require={
+        'lint': [
+            'flake8>=3.5.0',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'lsdfailtools-manage=lsdfailtools_web.manage:main',
+        ]
+    },
+    author=author,
+    description="web application for running lsdfailtools",
+)
