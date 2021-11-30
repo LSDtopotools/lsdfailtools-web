c8310c9c25b52cc2368a6592deadb549840359a8
mhagdorn <magnus.hagdorn@marsupium.org>
2021-11-30 17:16:09 +0000

    implement google login
     * lsdfailtools_web/application.py: add login manager
     * lsdfailtools_web/config.py: add google specific config
     * lsdfailtools_web/models.py: the User class also needs to inherit from
       the flask_login UserMixin object
     * lsdfailtools_web/routes.py: add the various routes required by the
       google login

--- lsdfailtools_web/application.py
+++ lsdfailtools_web/application.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from .config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
